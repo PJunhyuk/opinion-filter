fb068f3c57a4bd70dcf42fc8c100f63edb023416
PJunhyuk-mac <jgravity@naver.com>
2018-06-09 07:40:46 +0900

    Add Google Analytics

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,15 @@
   </div>
 </template>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-92632505-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-92632505-2');
+</script>
+
 <script>
 export default {
   name: 'App'
